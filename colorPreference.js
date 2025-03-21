6b44fc4c0605166ec77391dfa9075240ac3c2194
unija28 <cabellunesa@gmail.com>
2025-03-21 23:52:49 +0800

    colorPreference.js

---
+++ colorPreference.js
@@ -0,0 +1,7 @@
+let colors= [];
+
+for(let i = 0; i < 3; i++){
+    let FavoriteColors = prompt("Enter your favorite color");
+    colors.push(FavoriteColors);
+    console.log("Updated list of colors:", colors);
+}
