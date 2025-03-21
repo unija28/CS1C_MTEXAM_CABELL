db2280edb219083b0b171de1d32da1ce69bffe77
unija28 <cabellunesa@gmail.com>
2025-03-21 23:50:30 +0800

    classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,8 @@
+let studentName = prompt("Enter student name:");
+let subjectTitle = prompt("Enter subject title:");
+let classSchedule = prompt("Enter class schedule (Time, Days):");
+let classroom = prompt("Enter classroom:");
+let classInstuctor = prompt("Enter class instructor:");
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}.
+The instructor for the subject is ${classInstratuctor}`);
