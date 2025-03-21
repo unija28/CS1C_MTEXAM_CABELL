601727c31d5aafb9db52dec41e365352d767e721
unija28 <cabellunesa@gmail.com>
2025-03-21 23:49:27 +0800

    userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let FullName = prompt("Enter your full name");
+let Age = prompt("Enter your age");
+let FavNum = prompt("Enter your favorite number");
+let FavColor = prompt("Enter your favorite color");
+
+console.log("Full name:", FullName);
+console.log("Age:", Age);
+console.log("Favorite Number:", FavNum);
+console.log("Favorite Color:", FavColor);
